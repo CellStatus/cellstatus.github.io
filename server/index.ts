13a1481368eeb3c31e4a269514942ff0e360cc45
Robert Wayne White <rwaynewhite15@gmail.com>
2025-12-03 15:50:31 -0500

    Fix CORS: allow Cache-Control and Pragma headers

--- server/index.ts
+++ server/index.ts
@@ -36,7 +36,7 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Pragma"],
   })
 );
 
